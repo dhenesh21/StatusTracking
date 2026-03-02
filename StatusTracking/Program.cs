21150b89e0d8bfa27c25848ac58a87742f1c6561
dhenesh21 <dhenesh87@gmail.com>
2026-03-02 11:31:28 +0530

    Program.cs changes

--- StatusTracking/Program.cs
+++ StatusTracking/Program.cs
@@ -51,6 +51,15 @@ builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 // ? Swagger with JWT
 builder.Services.AddSwaggerGen(options =>
 {
@@ -89,6 +98,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAngular");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
